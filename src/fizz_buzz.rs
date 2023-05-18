b2e08815243908a33850762963150f010e30d3dd
Daniel Arteta <danielatts97@gmail.com>
2023-05-18 02:51:28 -0500

    refac: change file names

---
+++ src/fizz_buzz.rs
@@ -0,0 +1,10 @@
+pub fn print_fizz_buzz() -> () {
+    for num in 1..=100 {
+        match num {
+            num if num % 3 == 0 && num % 5 == 0 => println!("fizzbuzz"),
+            num if num % 3 == 0 => println!("fizz"),
+            num if num % 5 == 0 => println!("buzz"),
+            _ => println!("{num}"),
+        }
+    }
+}
