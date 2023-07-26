4d8a3de987b0d956176e365cbfae0430dce70914
Daniel Arteta <danielatts97@hotmail.com>
2023-07-25 23:00:31 -0500

    feat: add api call exercise

---
+++ src/apicall.rs
@@ -0,0 +1,30 @@
+//
+// Llamar a una API es una de las tareas más comunes en programación.
+//
+// Implementa una llamada HTTP a una API (la que tú quieras) y muestra su
+// resultado a través de la terminal. Por ejemplo: Pokémon, Marvel...
+//
+// Aquí tienes un listado de posibles APIs:
+// https://github.com/public-apis/public-apis
+
+use reqwest::Error;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Record {
+    description: String,
+    amount: u32,
+    id: u32,
+}
+
+#[tokio::main]
+pub async fn apicall() -> Result<(), Error> {
+    let request_url = format!("http://localhost:5000/api/records");
+    println!("{request_url}");
+    let response = reqwest::get(&request_url).await?;
+    let records: Vec<Record> = response.json().await?;
+    for elem in records {
+        println!("{:?}", elem);
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@
 mod hacker_language;
 // mod tennis_match;
 // mod password_generator;
+mod apicall;
 mod prime_fibonacci_even;
 mod rock_paper_scissors;
 mod text_type;
 
-fn main() {}
+fn main() {
+    let _ = apicall::apicall();
+}
