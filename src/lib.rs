48a073606d829d93194ee916492b17045bdf3029
Daniel Arteta <danielatts97@hotmail.com>
2023-07-26 08:34:49 -0500

    feat: add lib file

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+// mod fizz_buzz;
+mod hacker_language;
+// mod tennis_match;
+// mod password_generator;
+// mod apicall;
+mod prime_fibonacci_even;
+mod rock_paper_scissors;
+mod text_type;
+
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,1 @@
-// mod fizz_buzz;
-mod hacker_language;
-// mod tennis_match;
-// mod password_generator;
-mod apicall;
-mod prime_fibonacci_even;
-mod rock_paper_scissors;
-mod text_type;
-
-fn main() {
-    let _ = apicall::apicall();
-}
+fn main() {}
